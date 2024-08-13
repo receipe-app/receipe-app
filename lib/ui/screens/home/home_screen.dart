0415036130e31d4b328e5de97dbe3f0297ac697b
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-08-13 17:59:58 +0500

    Profile Sccreen added, updated

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:receipe_app/logic/bloc/auth/auth_bloc.dart';
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -9,13 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                context.read<AuthBloc>().add(const AuthEvent.logout());
-              },
-              icon: const Icon(Icons.logout))
-        ],
+        
       ),
       body: const Center(
         child: Text('Home'),
