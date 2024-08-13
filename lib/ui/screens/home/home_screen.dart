05fe03120bf2fd0cc1e95f0fef4373cff05a94b2
ibragimov05 <ibragimov.asus15@gmail.com>
2024-08-13 15:58:30 +0500

    commit

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,21 +9,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Text('Content for index $_selectedIndex'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.orange,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        icons: const [Icons.abc, Icons.abc, Icons.abc, Icons.abc],
-        activeIndex: 0,
-        gapLocation: GapLocation.center,
-        notchSmoothness: NotchSmoothness.smoothEdge,
-        onTap: (p0) {},
-        //other params
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildNavItem('assets/icon1.svg', 0),
+              _buildNavItem('assets/icon2.svg', 1),
+              SizedBox(width: 40), // Space for FAB
+              _buildNavItem('assets/icon3.svg', 2),
+              _buildNavItem('assets/icon4.svg', 3),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildNavItem(String assetName, int index) {
+    return IconButton(
+      icon: SvgPicture.asset(
+        assetName,
+        color: _selectedIndex == index ? Colors.orange : Colors.grey,
+      ),
+      onPressed: () => setState(() => _selectedIndex = index),
     );
   }
 }
