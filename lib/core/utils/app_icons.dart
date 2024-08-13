11c1f1bf0be71a72dc370871d3d5758d21d0c04e
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-08-13 15:23:20 +0500

    login register qisman qilindi unga dizayn va o'zgatirishlar kiritish kerak, figmadan color olindi iconlarni inactive lari olindi

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,23 @@
+class AppIcons {
+  static const String bookmarksInactive = 'assets/icons/bookmarksInactive.svg';
+  static const String homeInactive = 'assets/icons/homeInactive.svg';
+  static const String notificationInactive =
+      'assets/icons/notificationInactive.svg';
+  static const String settingsInactive = 'assets/icons/settingsInactive.svg';
+  static const String starInactive = 'assets/icons/starInactive.svg';
+  static const String settings2Iactive = 'assets/icons/settings2Inactive.svg';
+  static const String timerInacttive = 'assets/icons/timerInacttive.svg';
+  static const String searchInactive = 'assets/icons/searchInactive.svg';
+  static const String arrowrightInactivesvg =
+      'assets/icons/arrowrightInactive.svg';
+  static const String arrowdownInactive = 'assets/icons/arrowdownInactive.svg';
+  static const String arrowupInactive = 'assets/icons/arrowupInactive.svg';
+  static const String moreInactive = 'assets/icons/moreInactive.svg';
+  static const String documentextInactive =
+      'assets/icons/documentextInactive.svg';
+  static const String documentfavoriteInactive =
+      'assets/icons/documentfavoriteInactive.svg';
+  static const String messageInactive = 'assets/icons/messageInactive.svg';
+  static const String forwardInactive = 'assets/icons/forwardInactive.svg';
+  static const String profileInactive = 'assets/icons/profileInactive.svg';
+}
--- lib/ui/screens/auth/login_screen.dart
+++ lib/ui/screens/auth/login_screen.dart
@@ -1,13 +1,135 @@
+// LoginScreen
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:receipe_app/ui/widgets/custom_textfield.dart';
+
+import '../../../logic/bloc/auth/auth_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text('Login'),),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Hello,',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'Welcome Back!',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 40),
+                CustomTextField(
+                  controller: _emailController,
+                  label: 'Email',
+                  hint: 'Enter Email',
+                  isEmail: true,
+                ),
+                const SizedBox(height: 16),
+                CustomTextField(
+                  controller: _passwordController,
+                  label: 'Enter Password',
+                  hint: 'Enter Password',
+                  isPassword: true,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Forgot Password?',
+                        style: TextStyle(color: Colors.orange)),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                BlocConsumer<AuthBloc, AuthState>(
+                  listener: (context, state) {
+                    if (state.authStatus == AuthStatus.error) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(state.error ?? 'An error occurred')),
+                      );
+                    } else if (state.authStatus == AuthStatus.authenticated) {
+                      Navigator.pushReplacementNamed(context, '/home');
+                    }
+                  },
+                  builder: (context, state) {
+                    return state.authStatus == AuthStatus.loading
+                        ? const Center(child: CircularProgressIndicator())
+                        : SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  context.read<AuthBloc>().add(
+                                        AuthEvent.loginUser(
+                                          email: _emailController.text,
+                                          password: _passwordController.text,
+                                        ),
+                                      );
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              child: const Text('Sign In'),
+                            ),
+                          );
+                  },
+                ),
+                const SizedBox(height: 16),
+                const Center(child: Text('Or Sign in With')),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Image.asset('assets/google_icon.png', width: 24),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Image.asset('assets/facebook_icon.png', width: 24),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Center(
+                  child: RichText(
+                    text: const TextSpan(
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(text: "Don't have an account? "),
+                        TextSpan(
+                          text: 'Sign up',
+                          style: TextStyle(color: Colors.orange),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
