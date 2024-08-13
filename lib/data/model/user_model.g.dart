79355d96a0e365337e67ce7279f08c025abf34d9
ibragimov05 <ibragimov.asus15@gmail.com>
2024-08-13 13:57:47 +0500

    auth bloc and repository has been added

---
+++ lib/data/model/user_model.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      id: json['id'] as String,
+      uid: json['uid'] as String,
+      email: json['email'] as String,
+      savedRecipesId: (json['savedRecipesId'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      likedRecipesId: (json['likedRecipesId'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'uid': instance.uid,
+      'email': instance.email,
+      'savedRecipesId': instance.savedRecipesId,
+      'likedRecipesId': instance.likedRecipesId,
+    };
---
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,39 @@
+class User {
+  final String id;
+  String email;
+  String token;
+  String refreshToken;
+  DateTime expiresIn;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.token,
+    required this.refreshToken,
+    required this.expiresIn,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['localId'],
+      email: json['email'],
+      token: json['idToken'],
+      refreshToken: json['refreshToken'],
+      expiresIn: DateTime.now().add(
+        Duration(
+          seconds: int.parse(json['expiresIn']),
+        ),
+      ),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "localId": id,
+      "email": email,
+      "idToken": token,
+      "refreshToken": refreshToken,
+      "expiresIn": expiresIn.toString(),
+    };
+  }
+}
